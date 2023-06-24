f1596905d42848d5f26f38b4cbfffaba95209b62
Sajib Pandit <sajibpandit1999@gmail.com>
2023-06-24 11:04:23 +0600

    style complete

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,3 +1,6 @@
+import { GrRotateLeft, GrRotateRight } from "react-icons/gr";
+import { CgMergeHorizontal, CgMergeVertical } from "react-icons/cg";
+import { IoMdUndo, IoMdRedo, IoIosAdd, IoIosImage } from "react-icons/io";
 import "./style.scss";
 
 export default function Home() {
@@ -39,6 +42,56 @@ export default function Home() {
                   ))}
                 </div>
               </div>
+              <div className="filter_slider">
+                <div className="label_bar">
+                  <label htmlFor="range">Rotate</label>
+                  <span>100%</span>
+                </div>
+                <input type="range" name="" id="" />
+              </div>
+
+              <div className="rotate">
+                <label htmlFor="">Rotate & Flip</label>
+                <div className="icons">
+                  <div>
+                    <GrRotateLeft />
+                  </div>
+                  <div>
+                    <GrRotateRight />
+                  </div>
+                  <div>
+                    <CgMergeVertical />
+                  </div>
+                  <div>
+                    <CgMergeHorizontal />
+                  </div>
+                </div>
+              </div>
+              
+            </div>
+            <div className="reset">
+                <button>Reset</button>
+                <button className="save">Save Image</button>
+              </div>
+          </div>
+
+          <div className="image_section">
+            <div className="image">
+                <label htmlFor="choose">
+                    <IoIosImage/>
+                    <span>Choose Image</span>
+                </label>
+            </div>
+            <div className="image_select">
+              <button className="undo">
+                <IoMdUndo />
+              </button>
+              <button className="redo">
+                <IoMdRedo />
+              </button>
+              <button className="crop">Crop Image</button>
+              <label htmlFor="choose">Choose Image</label>
+              <input type="file" name="" id="choose" />
             </div>
           </div>
         </div>
